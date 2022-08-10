d6760ebf63190c1938d64ce575a40c80cee360e5
MikeKade <mike@mikda.com>
2022-08-10 18:07:50 +0000

    Updated for branch 0.6.3

--- start-docker-qumuloalerts.ps1
+++ start-docker-qumuloalerts.ps1
@@ -1,4 +1,4 @@
-$env:QUMULOALERTS_VERSION = "0.6.2"
+$env:QUMULOALERTS_VERSION = "0.6.3"
 $env:ALERTS_CONFIGDIR = "./config/alerts/"
 $env:CONSUMER_CONFIGDIR = "./config/consumer/"
 
--- start-docker-qumuloalerts.sh
+++ start-docker-qumuloalerts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export QUMULOALERTS_VERSION=0.6.2
+export QUMULOALERTS_VERSION=0.6.3
 export ALERTS_CONFIGDIR=$(pwd)/config/alerts/
 export CONSUMER_CONFIGDIR=$(pwd)/config/consumer/
 
