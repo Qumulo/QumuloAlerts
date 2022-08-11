9c6911772d51862ba811ade93525d3f80746731a
MikeKade <mike@mikda.com>
2022-08-11 01:19:31 +0000

    Updated for branch 0.6.3

--- stop-docker-qumuloalerts.sh
+++ stop-docker-qumuloalerts.sh
@@ -2,11 +2,22 @@
 
 # Uncomment if runnning in a Docker Swarm
 
-# docker stack rm storagereport
+# docker stack rm qumuloalerts
 
 # This stops a container running in a Docker stand-alone configuration
 # Comment out this group of lines if you need to stop a Docker Swarm
 
-docker container stop storagereport
-docker container rm storagereport
+docker container stop qumuloalerts-email-1
+docker container stop qumuloalerts-ifttt-1
+docker container stop qumuloalerts-alerts-1
+docker container stop qumuloalerts-exchange-1
+
+docker container rm qumuloalerts-email-1
+docker container rm qumuloalerts-ifttt-1
+docker container rm qumuloalerts-alerts-1
+docker container rm qumuloalerts-exchange-1
+
+# Finally, remove the shared network
+
+docker network rm qumuloalerts
 
