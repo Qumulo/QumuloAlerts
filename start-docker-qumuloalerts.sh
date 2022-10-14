68bb74672e42b44576d8339411118bb62a305907
MikeKade <mkade@qumulo.com>
2022-10-14 02:34:21 +0000

    Updated for branch 5.2.4

--- start-docker-qumuloalerts.sh
+++ start-docker-qumuloalerts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export QUMULOALERTS_VERSION=0.6.6
+export QUMULOALERTS_VERSION=5.2.4
 export ALERTS_CONFIGDIR=$(pwd)/config/alerts/
 export CONSUMER_CONFIGDIR=$(pwd)/config/consumer/
 
