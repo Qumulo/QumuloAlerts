16fd4c4b2bfe7b437994ef252fd0829f782d27b7
BeratUlualan <bulualan@qumulo.com>
2023-12-05 21:25:22 +0000

    Updated for branch 6.2.1

--- alerts-storage/sql/load_default_quota.sql
+++ alerts-storage/sql/load_default_quota.sql
@@ -3,6 +3,6 @@ use alerts;
 # Load the default quota thresholds
 
 insert into defaultquota
-       (quota_prefix, critical, error, warning)
+       (quota_prefix, admin_email, critical, error, warning)
    values
-       ('', 95, 85, 75);
+       ('', '', 95, 85, 75);
